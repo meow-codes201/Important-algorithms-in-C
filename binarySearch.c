8033f6538f74ea723175155cf8fd141320df6864
code web <humairat291@gmail.com>
2026-02-07 12:57:56 +0530

    Enhance binary search with debug output and fix array
    Updated binary search functions to include debug prints and modified the array to remove a duplicate element.

--- binarySearch.c
+++ binarySearch.c
@@ -5,12 +5,16 @@ int binaryIterative(int a[],int n,int key){
     int high = n-1;
     while(low<=high){
         int mid = (low+high)/2;
-        if(a[mid] == key)
+        if(a[mid] == key){
+            printf("FOUND IT!\n");
             return mid;
+        }
         else if(a[mid] > key){
+            printf("searching first half....\n");
             high = mid-1;
         }
         else{
+            printf("searching 2nd half....\n");
             
             low = mid +1;
         }
@@ -25,12 +29,15 @@ int binaryRecursive(int a[],int l,int h, int key){
     while(l <= h){
         int mid = (l+h)/2;
         if(a[mid]==key){
+            printf("FOUND IT!\n");
             return mid;
         }
         else if(a[mid]>key){
+            printf("searching first half....\n");
             return binaryRecursive(a,l,mid-1,key);
         }
         else{
+            printf("searching 2nd half....\n");
             return binaryRecursive(a,mid+1,h,key);
         }
     }
@@ -39,34 +46,62 @@ int binaryRecursive(int a[],int l,int h, int key){
 
 int main(){
     int n;
-    int a[] = {1,5,8,10,14,15};
+    int a[] = {1,5,8,10,15};
     int key = 14;
      n = sizeof(a)/sizeof(a[0]);
     int loc1 = binaryIterative(a,n,1);
+    printf("key = %d is at index = %d\n\n",1,loc1);
+
     int pos1 = binaryRecursive(a,0,n-1,15);
+    printf("key = %d is at index = %d\n\n",15,pos1);
+
     int loc2 = binaryIterative(a,n,15);
+    printf("key = %d is at index = %d\n\n",15,loc2);
+
     int pos2 = binaryRecursive(a,0,n-1,1);
+    printf("key = %d is at index = %d\n\n",1,pos2);
+
     int loc3 = binaryIterative(a,n,8);
+    printf("key = %d is at index = %d\n\n",8,loc3);
+
     int pos3 = binaryRecursive(a,0,n-1,10);
+    printf("key = %d is at index = %d\n\n",10,pos3);
+
     int pos4 = binaryRecursive(a,0,n-1,100);
-    
-    printf("key = %d is at index = %d\n",1,loc1);
-    printf("key = %d is at index = %d\n",15,pos1);
-    printf("key = %d is at index = %d\n",1,pos2);
-    printf("key = %d is at index = %d\n",15,loc2);
-    printf("key = %d is at index = %d\n",8,loc3);
-    printf("key = %d is at index = %d\n",10,pos3);
     if (pos4 == -1){
-        printf("Key does not exist\n");
+        printf("Key does not exist\n\n");
     }
     //OUTPUTS:
-        // key = 1 is at index = 0
-        // key = 15 is at index = 5
-        // key = 1 is at index = 0
-        // key = 15 is at index = 5
-        // key = 8 is at index = 2
-        // key = 10 is at index = 3
-        // Key does not exist
+        /*searching first half....
+        FOUND IT!
+        key = 1 is at index = 0
+        
+        searching 2nd half....
+        searching 2nd half....
+        FOUND IT!
+        key = 15 is at index = 4
+        
+        searching 2nd half....
+        searching 2nd half....
+        FOUND IT!
+        key = 15 is at index = 4
+        
+        searching first half....
+        FOUND IT!
+        key = 1 is at index = 0
+        
+        FOUND IT!
+        key = 8 is at index = 2
+        
+        searching 2nd half....
+        FOUND IT!
+        key = 10 is at index = 3
+        
+        searching 2nd half....
+        searching 2nd half....
+        searching 2nd half....
+        Key does not exist
+*/
     return 0;
     
 }
